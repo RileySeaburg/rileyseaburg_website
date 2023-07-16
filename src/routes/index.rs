ce0ba805f590721ae5b8c6dea3f9b2554c262967
rileyseaburg <riley@rileyseaburg.com>
2023-07-15 23:22:24 -0400

    fix: Updated index route to use the correct context value
    The index route in the index.rs file was updated to use the correct context value for the route name, instead of using a placeholder value. This change ensures that the rendered HTML template includes the correct route name.

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,10 +1,10 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{get, web, HttpResponse};
 use tera::{Context, Tera};
 
 #[get("/")]
-async fn index(tmpl: web::Data<Tera>) -> HttpResponse {
+pub async fn index(tmpl: web::Data<Tera>) -> HttpResponse {
     let mut context = Context::new();
-    context.insert("foo", "123");
+    context.insert("route_name", "index");
     let rendered = tmpl.render("pages/index.html.tera", &context).unwrap();
     HttpResponse::Ok().body(rendered)
 }
