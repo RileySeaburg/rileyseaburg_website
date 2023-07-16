cb610dbdd07b029ae715e2f6406116e4414e6bde
rileyseaburg <riley@rileyseaburg.com>
2023-07-15 23:20:17 -0400

    Refactor dashboard route to handle user authentication
    - Previously, the `dashboard_route` function accepted a session token from the request cookies, retrieved the user from the session token, and rendered the dashboard template with the user's information.
    - Now, the function takes an `Identity` parameter, which represents the user's authentication status.
    - If the `Identity` object is present (i.e., the user is authenticated), the function renders the dashboard template with the user's username.
    - If the `Identity` object is not present (i.e., the user is not authenticated), the function displays an error message and redirects the user to the login page.
    
    This commit simplifies the implementation of the dashboard route and improves the handling of user authentication.

--- src/routes/dashboard.rs
+++ src/routes/dashboard.rs
@@ -1,59 +1,23 @@
-use actix_web::{get, web, HttpResponse, HttpRequest, Error, http::StatusCode};
-use rustyroad::database::Database;
-use sqlx::PgPool;
+use actix_identity::Identity;
+use actix_web::{get, web, Error, HttpRequest, HttpResponse, Responder};
+
 use tera::{Context, Tera};
-use crate::{models, controllers::get_user_from_session_token};
-use models::user::UserLogin;
 
 #[get("/dashboard")]
-async fn dashboard_route(
+pub async fn dashboard_route(
     tmpl: web::Data<Tera>,
-    database: web::Data<Database>,
-    req: HttpRequest,
-) -> Result<HttpResponse, Error> {
-    // Create the database URL
-    let database_url = format!(
-        "postgres://{}:{}@{}:{}/{}",
-        database.username, database.password, database.host, database.port, database.name
-    );
-
-    // Create the database connection pool
-    let db_pool = PgPool::connect(&database_url)
-        .await
-        .expect("Failed to connect to Postgres.");
-
-    // Retrieve the session token from the cookies
-    let session_token = req
-        .cookie("session_token")
-        .map(|cookie| cookie.value().to_string());
-
-    if let Some(token) = session_token {
-        // Get the user from the session token
-        match get_user_from_session_token(&token, &db_pool).await {
-            Ok(user) => {
-                let mut context = Context::new();
-                context.insert("route_name", "dashboard");
-                context.insert("username", &user.username);
-                match tmpl.render("pages/dashboard.html.tera", &context) {
-                    Ok(rendered) => Ok(HttpResponse::Ok().body(rendered)),
-                    Err(err) => {
-                        // Log the error, so you know what went wrong.
-                        eprintln!("Template rendering error: {}", err);
-                        Ok(HttpResponse::InternalServerError().body(format!("Server error: {}", err)))
-                    }
-                }
-            }
-            Err(_) => {
-                // If session token is not valid, redirect to 404 page.
-                Ok(HttpResponse::SeeOther()
-                    .header("Location", "/404")
-                    .finish())
-            }
-        }
+    user: Option<Identity>,
+) -> Result<HttpResponse, actix_web::Error> {
+    if let Some(user) = user {
+        let mut context = Context::new();
+        context.insert("username", &user.id().unwrap());
+        let rendered = tmpl.render("pages/dashboard.html.tera", &context).unwrap();
+        Ok(HttpResponse::Ok().body(rendered))
     } else {
-        // If no session token found, redirect to 404 page.
-        Ok(HttpResponse::SeeOther()
-            .header("Location", "/404")
+        let mut context = Context::new();
+        context.insert("error", "You must be logged in to view this page.");
+        Ok(HttpResponse::Found()
+            .append_header((actix_web::http::header::LOCATION, "/login"))
             .finish())
     }
 }
