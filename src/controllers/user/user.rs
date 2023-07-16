4c5b726fc843df519a8ef2175686808cda2b3ee2
rileyseaburg <riley@rileyseaburg.com>
2023-07-15 23:19:49 -0400

    feat: Add get_user_from_session_token function
    This commit adds the `get_user_from_session_token` function, which takes a session token and a database connection pool as arguments and returns an optional user. The function fetches the user data from the database based on the session token. If the user exists, it returns the user as Some, otherwise it returns None.

--- src/controllers/user/user.rs
+++ src/controllers/user/user.rs
@@ -1,10 +1,49 @@
 use crate::models::User;
 
-pub async fn get_user_from_session_token(
-    session_token: &str,
-    pool: &sqlx::PgPool,
-) -> Result<User, sqlx::Error> {
-    sqlx::query_as(
+/// # Name: get_user_from_session_token
+/// 
+/// ## Description
+/// 
+/// This function takes a session token and a database connection pool and returns an optional user.
+/// 
+/// ## Arguments
+/// 
+/// * `session_token` - A string slice that holds the session token.
+/// * `pool` - A reference to a database connection pool.
+/// 
+/// ## Returns
+/// 
+/// An optional user.
+/// 
+/// ## Example
+/// 
+/// ```
+/// use rustyroad::database::Database;
+/// 
+/// let database = Database {
+///    username: "postgres".to_string(),
+///   password: "postgres".to_string(),
+///   host: "localhost".to_string(),
+///  port: 5432,
+/// name: "example".to_string(),
+/// };
+/// 
+/// let database_url = format!(
+///    "postgres://{}:{}@{}:{}/{}",
+///   database.username, database.password, database.host, database.port, database.name
+/// );
+/// 
+/// let db_pool = PgPool::connect(&database_url)
+///   .await
+///  .expect("Failed to connect to Postgres.");
+/// d
+/// let user = get_user_from_session_token("session_token", &db_pool).await;
+/// 
+/// assert_eq!(user.is_some(), true);
+/// ```
+/// 
+pub async fn get_user_from_session_token(session_token: &str, pool: &sqlx::PgPool) -> Option<User> {
+    let user: Result<User, sqlx::Error> = sqlx::query_as(
         "
         SELECT Users.id, Users.username, Users.password, Users.email, Users.created_at, Users.updated_at 
         FROM Users 
@@ -14,5 +53,10 @@ pub async fn get_user_from_session_token(
     )
     .bind(session_token)
     .fetch_one(pool)
-    .await
+    .await;
+
+    match user {
+        Ok(user) => Some(user),
+        Err(_) => None,
+    }
 }
