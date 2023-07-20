b3e1220642a5b4fc598d829e33ea468911fda679
rileyseaburg <riley@rileyseaburg.com>
2023-07-20 01:36:16 -0400

    add newsletter route post

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -20,43 +20,43 @@ async fn newsletter_post(form: web::Form<NewsletterForm>) -> impl Responder {
     print!("{}", email);
     let newSubscriber = Subscriber::new(email);
 
-    // // insert the new subscriber into the database
-    // let database = web::Data::new(Database::get_database_from_rustyroad_toml().unwrap());
+    // insert the new subscriber into the database
+    let database = web::Data::new(Database::get_database_from_rustyroad_toml().unwrap());
 
-    // // Create the database URL
-    // let database_url = format!(
-    //     "postgres://{}:{}@{}:{}/{}",
-    //     database.username, database.password, database.host, database.port, database.name
-    // );
+    // Create the database URL
+    let database_url = format!(
+        "postgres://{}:{}@{}:{}/{}",
+        database.username, database.password, database.host, database.port, database.name
+    );
 
-    // let mut conn = PgPool::connect(&database_url)
-    //     .await
-    //     .expect("Failed to connect to Postgres.");
+    let mut conn = PgPool::connect(&database_url)
+        .await
+        .expect("Failed to connect to Postgres.");
 
-    // let result = newSubscriber.insert(&mut conn);
+    let result = newSubscriber.insert(&mut conn);
 
-    // #[derive(serde::Serialize, serde::Deserialize)]
-    // struct JSONResoponse {
-    //     message: String,
-    // }
+    #[derive(serde::Serialize, serde::Deserialize)]
+    struct JSONResoponse {
+        message: String,
+    }
 
-    // let mut json_response = JSONResoponse {
-    //     message: "There was an error subscribing to the newsletter.".to_string(),
-    // };
+    let mut json_response = JSONResoponse {
+        message: "There was an error subscribing to the newsletter.".to_string(),
+    };
 
-    // match result.await {
-    //     Ok(_) => {
-    //         json_response.message =
-    //             "You have successfully subscribed to the newsletter.".to_string()
-    //     }
-    //     Err(_) => {
-    //         json_response.message = "There was an error subscribing to the newsletter.".to_string()
-    //     }
-    // }
+    match result.await {
+        Ok(_) => {
+            json_response.message =
+                "You have successfully subscribed to the newsletter.".to_string()
+        }
+        Err(_) => {
+            json_response.message = "There was an error subscribing to the newsletter.".to_string()
+        }
+    }
 
     // create json response for the user
 
     HttpResponse::Ok()
         .content_type("application/json")
-        .json(form)
+        .json(json_response)
 }
