783efe8908771451d2fe851f6876203076e78e4a
rileyseaburg <riley@rileyseaburg.com>
2023-07-20 01:04:03 -0400

    add jspn response.

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -1,8 +1,8 @@
+use crate::models::{NewsletterForm, Subscriber};
 use actix_web::{get, post, web, HttpResponse, Responder};
 use rustyroad::database::Database;
 use sqlx::PgPool;
 use tera::{Context, Tera};
-use crate::models::{NewsletterForm, Subscriber};
 
 #[get("/newsletter")]
 async fn newsletter(tmpl: web::Data<Tera>) -> impl Responder {
@@ -13,7 +13,7 @@ async fn newsletter(tmpl: web::Data<Tera>) -> impl Responder {
 }
 
 #[post("/newsletter")]
-async fn newsletter_post(tmpl: web::Data<Tera>, form: web::Form<NewsletterForm>) -> impl Responder {
+async fn newsletter_post(form: web::Form<NewsletterForm>) -> impl Responder {
     let mut context = Context::new();
     context.insert("route_name", "newsletter");
 
@@ -21,7 +21,6 @@ async fn newsletter_post(tmpl: web::Data<Tera>, form: web::Form<NewsletterForm>)
     let email = &form.email.to_string();
     print!("{}", email);
     let newSubscriber = Subscriber::new(email);
-    
 
     // insert the new subscriber into the database
     let database = web::Data::new(Database::get_database_from_rustyroad_toml().unwrap());
@@ -29,11 +28,7 @@ async fn newsletter_post(tmpl: web::Data<Tera>, form: web::Form<NewsletterForm>)
     // Create the database URL
     let database_url = format!(
         "postgres://{}:{}@{}:{}/{}",
-        database.username,
-        database.password,
-        database.host,
-        database.port,
-        database.name
+        database.username, database.password, database.host, database.port, database.name
     );
 
     let mut conn = PgPool::connect(&database_url)
@@ -44,14 +39,20 @@ async fn newsletter_post(tmpl: web::Data<Tera>, form: web::Form<NewsletterForm>)
 
     match result.await {
         Ok(_) => {
-            context.insert("message", "You have successfully subscribed to the newsletter.");
+            context.insert(
+                "message",
+                "You have successfully subscribed to the newsletter.",
+            );
         }
         Err(_) => {
-            context.insert("message", "There was an error subscribing to the newsletter.");
+            context.insert(
+                "message",
+                "There was an error subscribing to the newsletter.",
+            );
         }
     }
 
+    // create json response for the user
 
-    let rendered = tmpl.render("pages/newsletter.html.tera", &context).unwrap();
-    HttpResponse::Ok().body(rendered)
+    HttpResponse::Ok().json(&context)
 }
