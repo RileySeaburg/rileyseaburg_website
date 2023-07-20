17ea2cf971c25fef703885892e71d8ee19f58b60
rileyseaburg <riley@rileyseaburg.com>
2023-07-20 01:35:01 -0400

    add newsletter route post

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -14,51 +14,49 @@ async fn newsletter(tmpl: web::Data<Tera>) -> impl Responder {
 
 #[post("/newsletter")]
 async fn newsletter_post(form: web::Form<NewsletterForm>) -> impl Responder {
-    let mut context = Context::new();
-    context.insert("route_name", "newsletter");
-
+   
     // extract email from the form
     let email = &form.email.to_string();
     print!("{}", email);
     let newSubscriber = Subscriber::new(email);
 
-    // insert the new subscriber into the database
-    let database = web::Data::new(Database::get_database_from_rustyroad_toml().unwrap());
+    // // insert the new subscriber into the database
+    // let database = web::Data::new(Database::get_database_from_rustyroad_toml().unwrap());
 
-    // Create the database URL
-    let database_url = format!(
-        "postgres://{}:{}@{}:{}/{}",
-        database.username, database.password, database.host, database.port, database.name
-    );
+    // // Create the database URL
+    // let database_url = format!(
+    //     "postgres://{}:{}@{}:{}/{}",
+    //     database.username, database.password, database.host, database.port, database.name
+    // );
 
-    let mut conn = PgPool::connect(&database_url)
-        .await
-        .expect("Failed to connect to Postgres.");
+    // let mut conn = PgPool::connect(&database_url)
+    //     .await
+    //     .expect("Failed to connect to Postgres.");
 
-    let result = newSubscriber.insert(&mut conn);
+    // let result = newSubscriber.insert(&mut conn);
 
-    #[derive(serde::Serialize, serde::Deserialize)]
-    struct JSONResoponse {
-        message: String,
-    }
+    // #[derive(serde::Serialize, serde::Deserialize)]
+    // struct JSONResoponse {
+    //     message: String,
+    // }
 
-    let mut json_response = JSONResoponse {
-        message: "There was an error subscribing to the newsletter.".to_string(),
-    };
+    // let mut json_response = JSONResoponse {
+    //     message: "There was an error subscribing to the newsletter.".to_string(),
+    // };
 
-    match result.await {
-        Ok(_) => {
-            json_response.message =
-                "You have successfully subscribed to the newsletter.".to_string()
-        }
-        Err(_) => {
-            json_response.message = "There was an error subscribing to the newsletter.".to_string()
-        }
-    }
+    // match result.await {
+    //     Ok(_) => {
+    //         json_response.message =
+    //             "You have successfully subscribed to the newsletter.".to_string()
+    //     }
+    //     Err(_) => {
+    //         json_response.message = "There was an error subscribing to the newsletter.".to_string()
+    //     }
+    // }
 
     // create json response for the user
 
     HttpResponse::Ok()
         .content_type("application/json")
-        .json(json_response)
+        .json(form)
 }
