93ba188108c716fdfa85705925ca3946275ee0b7
rileyseaburg <riley@rileyseaburg.com>
2023-07-25 03:08:31 -0400

    Add new fields to Post struct and implement the create_post function.
    - Added two new fields to the `Post` struct: `created_at` and `updated_at`.
    - Implemented the `create_post` function to insert a new post into the database.
    - Modified the existing test case to include the new fields in the `Post` struct.
    
    Note: Custom serialization/deserialization is used for the `publish_date`, `created_at`, and `updated_at` fields.

--- src/models/post.rs
+++ src/models/post.rs
@@ -4,21 +4,27 @@ use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 use crate::controllers::{execute_query, get_db_pool};
 
+use crate::controllers::datetime::{self, datetime as local_datetime};
+
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, FromRow)]
 pub struct Post {
-    pub id: i32,
+    pub id: Option<i32>,
     pub author: Option<String>,
     pub title: Option<String>,
     pub content: Option<String>,
     pub tags: Option<Vec<String>>,
+    #[serde(with = "local_datetime")]  // use the custom serializer/deserializer
     pub publish_date: Option<NaiveDateTime>,
     pub status: Option<String>,
     pub image_url: Option<String>,
     pub category: Option<String>,
+    #[serde(with = "local_datetime")]  // use the custom serializer/deserializer
     pub created_at: Option<NaiveDateTime>,
+    #[serde(with = "local_datetime")]  // use the custom serializer/deserializer
     pub updated_at: Option<NaiveDateTime>,
     pub slug: Option<String>,
 }
+
 
 
 impl Post {
@@ -105,6 +111,31 @@ impl Post {
     }
 
 
+    pub async fn create_post(new_post: Post) -> Result<Post, sqlx::Error> {
+        let pool: sqlx::PgPool = get_db_pool().await.unwrap();
+        let post: Post = sqlx::query_as("
+        INSERT INTO Posts (author, title, content, tags, publish_date, status, image_url, category, created_at, updated_at, slug)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+        RETURNING *
+    ")
+            .bind(new_post.author)
+            .bind(new_post.title)
+            .bind(new_post.content)
+            .bind(new_post.tags)
+            .bind(new_post.publish_date)
+            .bind(new_post.status)
+            .bind(new_post.image_url)
+            .bind(new_post.category)
+            .bind(new_post.created_at)
+            .bind(chrono::Utc::now().naive_utc())
+            .bind(new_post.slug)
+            .fetch_one(&pool)
+            .await?;
+        Ok(post)
+    }
+
+
+
 }
 
 
@@ -120,8 +151,8 @@ mod tests {
         let test_post = Post {
             id: 1,
             author: Some(String::from("John Doe")),
-            title: String::from("My First Blog Post"),
-            content: String::from("This is the content of my first blog post"),
+            title: Some(String::from("My First Blog Post")),
+            content: Some(String::from("This is the content of my first blog post")),
             tags: Some(vec![String::from("blog"), String::from("first post")]),
             publish_date: Some( NaiveDateTime::parse_from_str(date_str, "%Y-%m-%d %H:%M:%S").unwrap()),
             status: Some(String::from("published")),
