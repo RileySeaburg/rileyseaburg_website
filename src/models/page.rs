7065bc34d9c11405209cf217e6d8ac6a93427074
rileyseaburg <riley@rileyseaburg.com>
2023-07-30 00:51:21 -0400

    Add Page model to handle page-specific data
    The commit adds a Page struct in src/models/page.rs to store page-related information such as id, title, slug, content, author, meta description, SEO image, created at, and updated at.

---
+++ src/models/page.rs
@@ -0,0 +1,13 @@
+
+
+pub struct Page {
+    pub id: i32,
+    pub title: String,
+    pub slug: String,
+    pub content: String,
+    pub author: String,
+    pub meta_description: String,
+    pub seo_image: String,
+    pub created_at: String,
+    pub updated_at: String,
+}
