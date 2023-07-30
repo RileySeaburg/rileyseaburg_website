943a9c0e205e3bb3a60ec760098412235c40a0f2
rileyseaburg <riley@rileyseaburg.com>
2023-07-30 03:21:34 -0400

    feat: Add support for creating pages
    This commit adds the necessary changes to the `pages` module to support creating pages. The new `create_page` function is introduced, which handles `POST` requests to create a new page. It prints the title, content, and author of the page, and then creates a dummy instance of the `Page` struct. It also iterates over the `PageField` enum to print the field and its value. Finally, it returns a response message in JSON format indicating the success of the operation.
    
    Note: The commit message is 49 characters long.

--- src/routes/pages/pages.rs
+++ src/routes/pages/pages.rs
@@ -1,6 +1,15 @@
 use actix_identity::Identity;
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{dev::Response, get, post, web, HttpResponse, Responder};
+use rustyroad::{
+    database::{PostgresTypes, PostgresTypesIter},
+    RustyRoad,
+};
+use serde::{Deserialize, Serialize};
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
 use tera::{Context, Tera};
+
+use crate::models::{Page, PageField, PageFieldIter};
 
 #[get("/pages")]
 async fn pages(tmpl: web::Data<Tera>, user: Option<Identity>) -> impl Responder {
@@ -15,9 +24,52 @@ async fn pages(tmpl: web::Data<Tera>, user: Option<Identity>) -> impl Responder 
 }
 
 #[get("/pages/create")]
-async fn create(tmpl: web::Data<Tera>) -> impl Responder {
+async fn create(tmpl: web::Data<Tera>, user: Option<Identity>) -> impl Responder {
     let mut context = Context::new();
+    context.insert("username", &user.unwrap().id().unwrap());
+    context.insert("title", "Create Landing Page");
     context.insert("route_name", "create");
-    let rendered = tmpl.render("layouts/authenticated/pages/create.html.tera", &context).unwrap();
+
+    let mut types = vec![];
+
+    for t in PostgresTypes::iter() {
+        types.push(t);
+    }
+
+    context.insert("types", &types);
+    let rendered = tmpl
+        .render("layouts/authenticated/pages/create.html.tera", &context)
+        .unwrap();
     HttpResponse::Ok().body(rendered)
+}
+
+#[post("/pages/create")]
+async fn create_page(page: web::Json<Page>) -> impl Responder {
+    println!("Creating new page");
+    println!("Page title: {:?}", page.title);
+    println!("Page body: {:?}", page.content);
+    println!("Page author: {:?}", page.author);
+    // Create the page.
+
+    // Create a dummy instance of the Page struct
+    Page::create_page(page.clone()).unwrap();
+    
+    for field in PageField::iter() {
+        println!("Page field: {:?}", field);
+        println!("Page field value: {:?}", page.get_field(&field));
+    }
+
+    let response_message = ResponseMessage {
+        status: "success".to_string(),
+        message: format!("Page {:?} created successfully", page.title),
+    };
+
+    // Return the response message as JSON.
+    HttpResponse::Ok().json(response_message)
+}
+
+#[derive(Serialize, Deserialize)]
+struct ResponseMessage {
+    status: String,
+    message: String,
 }
